b5e0928d5bd56d0a78b1adad9aa2bb6b30bda1f8
Marley Nicolson <marley.nicolson@onqor.co.uk>
2022-10-28 12:54:57 +0100

    login auth response working

--- strapi-nuxt-boilerplate/frontend/pages/index.vue
+++ strapi-nuxt-boilerplate/frontend/pages/index.vue
@@ -1,49 +1,58 @@
 <template>
-  <div>
-    <BlockBuilder v-for="(item, index) in this.blocks" :key="index" :blockComponent="item" />
+  <div class="pkthunder-login">
+    <!-- <BlockBuilder v-for="(item, index) in this.blocks" :key="index" :blockComponent="item" /> -->
+    <h1>
+      welcome to PK thunder
+    </h1>
+    <p>please sign in to view and edit your details</p>
+
+    <input type="text" placeholder="Name" v-model="name">
+
+    <input type="text" placeholder="Password" v-model="password">
+
+    <div class="pkthunder-login__button" v-on:click="accsessControl()">
+      <p>login</p>
+    </div>
+    <img src="/images/pkThunder.jpg" />
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-export default {
-  name: 'HomePage', 
-  data(){
-    return {
-      page: this.$route.params.page,
-      pageID:'',
-      blocks: ''
-    }
-  },
-  methods: {
-    async asyncData() {
-      const thePageID = await fetch(
-          'http://localhost:1337/api/pages?filters[isHome]=true'
-          // 'http://localhost:1337/api/pages/1?populate=dynamic_content'
-      ).then((res) => {
-        // can set up 404 redirection here
-      return res.json();
-      });
-      var pageID = thePageID.data[0].id;
-
-      const thePageData = await fetch(
-          'http://localhost:1337/api/pages/' + pageID + '?populate[dynamic_content][populate]=*'
-        ).then((res) => {
-        // can set up 404 redirection here
-        return res.json();
-      });
-
-      console.log('thePageData', thePageData);
-      if(thePageData.data.attributes.dynamic_content){
-        this.blocks = thePageData.data.attributes.dynamic_content
+  import axios from 'axios';
+  export default {
+    name: 'HomePage', 
+    data(){
+      return {
+        page: this.$route.params.page,
+        pageID:'',
+        blocks: '',
+        name: '',
+        password: '',
+        theUserData: '',
       }
-      // console.log(this.$router.params);
-    }
-  },
-
-  mounted(){
-    this.asyncData();
+    },
+    methods: {
+      accsessControl(){
+        console.log('click', this.user);
+        axios.post('http://localhost:1337/api/auth/local', {
+            identifier: this.name,
+            password: this.password,
+        })
+        .then(response => {
+            // Handle success.
+            this.enter = true;
+            this.theApiKey = 'Bearer ' + response.data.jwt
+            console.log('enter', response );
+            this.theUserData = response;
+        })
+        .catch(error => {
+            // Handle error.
+            this.error = true;
+        });
+      }
+    },
+    beforeMount(){
+        require('@/assets/scss/login.scss');
+    },
   }
-  
-}
 </script>
